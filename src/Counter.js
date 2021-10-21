f7fe06dc5a8243634cef446982c0be512046d9a9
Aleksandr Abramov <aleksabramov77@gmail.com>
2021-10-21 23:15:00 +0300

    03 class Counter and class component Counter

--- src/Counter.js
+++ src/Counter.js
@@ -2,16 +2,20 @@ import React from 'react'
 import counter from './store/counter'
 import { observer } from 'mobx-react'
 
-const Counter = observer(() => {
 
-    return (
-        <div className="Counter">
-            <h1>{counter.count}</h1>
-            <button onClick={() => { counter.decrement() }}>-1</button>
-            <button onClick={() => counter.increment()}>+1</button>
-        </div>
-    )
+@observer
+class Counter extends React.Component {
+    render () {
 
-})
+        return (
+            <div className="Counter">
+                <h1>{counter.count}</h1>
+                <button onClick={() => { counter.decrement() }}>-1</button>
+                <button onClick={() => counter.increment()}>+1</button>
+            </div>
+        )
+
+    }
+}
 
 export default Counter
